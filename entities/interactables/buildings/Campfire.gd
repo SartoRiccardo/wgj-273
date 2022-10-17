1baab7afba4c2008eac51a7ef133951b049f7baa
SartoRiccardo <rikki.sarto@gmail.com>
2022-10-17 19:58:11 +0200

    Light decays with campfire now

--- entities/interactables/buildings/Campfire.gd
+++ entities/interactables/buildings/Campfire.gd
@@ -5,7 +5,9 @@ func _ready():
 		$AoE.queue_free()
 
 func _decay():
-	$Effect.queue_free()
+	var free = [$Effect, $Particles2D]
+	for node in free:
+		node.queue_free()
 	$AnimationPlayer.play("decay")
 	$Sprite.set_modulate(Color(1, 1, 1, 0.5))
 	delete_tooltip()
