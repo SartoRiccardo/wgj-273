8a3bb8aa0570a0298a82e36678914f3bdb7cfed1
SartoRiccardo <rikki.sarto@gmail.com>
2022-10-20 20:16:23 +0200

    Death screen goes back to main menu (#17)

--- ui/DeathScreen.gd
+++ ui/DeathScreen.gd
@@ -1,5 +1,11 @@
 extends PanelContainer
 
+onready var menu_btn = $Control/PanelContainer/MarginContainer/VBoxContainer/Button
+var menu_scene = load("res://ui/MainMenu.tscn")
+
+func _ready():
+	menu_btn.connect("pressed", self, "_on_menu")
+
 func set_time_survived(years, months):
 	var survive_stats = $Control/PanelContainer/MarginContainer/VBoxContainer/SurviveTime
 	if years > 0:
@@ -8,3 +14,6 @@ func set_time_survived(years, months):
 		survive_stats.text += "and"
 	if years == 0 or months > 0:
 		survive_stats.text += "%s months" % months
+
+func _on_menu():
+	Helpers.change_scene_to(menu_scene.instance())
